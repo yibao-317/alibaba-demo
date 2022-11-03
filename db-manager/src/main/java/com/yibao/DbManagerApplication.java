858035794bb02a7ab4abfc14f86887de4046b9a4
liyi <liyi@glasssix.com>
2022-11-03 16:21:19 +0800

    db init

---
+++ db-manager/src/main/java/com/yibao/DbManagerApplication.java
@@ -0,0 +1,16 @@
+package com.yibao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yibao
+ * @create 2022 -11 -03
+ * @desc
+ */
+@SpringBootApplication
+public class DbManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbManagerApplication.class, args);
+    }
+}
