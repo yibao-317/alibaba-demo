fe92d6646335e532c7ac64b34d0c5a985f262fdd
liyi <liyi@glasssix.com>
2022-11-14 18:10:15 +0800

    Nacos

---
+++ order/src/main/java/com/yibao/order/controller/NacosController.java
@@ -0,0 +1,48 @@
+package com.yibao.order.controller;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
+
+/**
+ * @author yibao
+ * @create 2022 -11 -14
+ * @desc nacos 测试代码
+ */
+@RestController
+@RequestMapping("/nacos")
+public class NacosController {
+
+    /**
+     * 方法：测试nacos配置管理是否能获取值
+     */
+    @GetMapping("/testConfig")
+    public void testConfig() {
+        try {
+            // 1.服务地址
+            String serverAddr = "127.0.0.1:8848";
+            // 2. data id
+            String dataId = "common.yml";
+            // 3.group
+            String group = "DEFAULT_GROUP";
+            // 4.namespace
+            String namespace = "33f134a8-bc10-4e97-8eff-1f4c930b732c";
+            // 5.获取配置
+            Properties properties = new Properties();
+            properties.put("serverAddr", serverAddr);
+            properties.put("namespace", namespace);
+
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            String config = configService.getConfig(dataId, group, 5000);
+            System.out.println(config);
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- user/src/main/java/com/yibao/user/UserCenterApplication.java
+++ user/src/main/java/com/yibao/user/UserCenterApplication.java
@@ -3,11 +3,9 @@ package com.yibao.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan(value = "com.yibao.user.mapper")
-@EnableDiscoveryClient
 public class UserCenterApplication {
 
 	public static void main(String[] args) {
