5a29f806336e81702246d67ed10d5665e904adfa
liyi <liyi@glasssix.com>
2022-11-14 11:25:52 +0800

    nacos

--- common/src/main/java/com/yibao/common/util/AutoCodeGenerator.java
+++ common/src/main/java/com/yibao/common/util/AutoCodeGenerator.java
@@ -1,13 +1,5 @@
 package com.yibao.common.util;
 
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
-import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.rules.DateType;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-
 /**
  * @author yibao
  * @create 2022 -11 -11
@@ -19,59 +11,59 @@ public class AutoCodeGenerator {
      */
     public static void autoCode() {
         // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java");
-        //作者
-        gc.setAuthor("yibao");
-        //打开输出目录
-        gc.setOpen(false);
-        //xml开启 BaseResultMap
-        gc.setBaseResultMap(true);
-        //xml 开启BaseColumnList
-        gc.setBaseColumnList(true);
-        //日期格式，采用Date
-        gc.setDateType(DateType.ONLY_DATE);
-        mpg.setGlobalConfig(gc);
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/yibao?useUnicode=true&characterEncoding=UTF-8");
-        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("123456");
-        mpg.setDataSource(dsc);
-
-        // 包配置
-        PackageConfig pc = new PackageConfig();
-        pc.setParent("com.yibao")
-                .setEntity("entity")
-//                .setMapper("mapper")
-//                .setService("service")
-//                .setServiceImpl("service.impl")
-//                .setController("controller")
-        ;
-        mpg.setPackageInfo(pc);
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        //数据库表映射到实体的命名策略
-        strategy.setNaming(NamingStrategy.underline_to_camel);
-        //数据库表字段映射到实体的命名策略
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        //lombok模型
-        strategy.setEntityLombokModel(true);
-        //生成 @RestController 控制器
-        // strategy.setRestControllerStyle(true);
-
-        // 数据库表名
-        strategy.setInclude("user","order");
-        strategy.setControllerMappingHyphenStyle(true);
-        //表前缀
-//        strategy.setTablePrefix("t_");
-        mpg.setStrategy(strategy);
-//        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+//        AutoGenerator mpg = new AutoGenerator();
+//        // 全局配置
+//        GlobalConfig gc = new GlobalConfig();
+//        String projectPath = System.getProperty("user.dir");
+//        gc.setOutputDir(projectPath + "/src/main/java");
+//        //作者
+//        gc.setAuthor("yibao");
+//        //打开输出目录
+//        gc.setOpen(false);
+//        //xml开启 BaseResultMap
+//        gc.setBaseResultMap(true);
+//        //xml 开启BaseColumnList
+//        gc.setBaseColumnList(true);
+//        //日期格式，采用Date
+//        gc.setDateType(DateType.ONLY_DATE);
+//        mpg.setGlobalConfig(gc);
+//        // 数据源配置
+//        DataSourceConfig dsc = new DataSourceConfig();
+//        dsc.setUrl("jdbc:mysql://localhost:3306/yibao?useUnicode=true&characterEncoding=UTF-8");
+//        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+//        dsc.setUsername("root");
+//        dsc.setPassword("123456");
+//        mpg.setDataSource(dsc);
+//
+//        // 包配置
+//        PackageConfig pc = new PackageConfig();
+//        pc.setParent("com.yibao")
+//                .setEntity("entity")
+////                .setMapper("mapper")
+////                .setService("service")
+////                .setServiceImpl("service.impl")
+////                .setController("controller")
+//        ;
+//        mpg.setPackageInfo(pc);
+//
+//        // 策略配置
+//        StrategyConfig strategy = new StrategyConfig();
+//        //数据库表映射到实体的命名策略
+//        strategy.setNaming(NamingStrategy.underline_to_camel);
+//        //数据库表字段映射到实体的命名策略
+//        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        //lombok模型
+//        strategy.setEntityLombokModel(true);
+//        //生成 @RestController 控制器
+//        // strategy.setRestControllerStyle(true);
+//
+//        // 数据库表名
+//        strategy.setInclude("user","order");
+//        strategy.setControllerMappingHyphenStyle(true);
+//        //表前缀
+////        strategy.setTablePrefix("t_");
+//        mpg.setStrategy(strategy);
+////        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+//        mpg.execute();
     }
 }
