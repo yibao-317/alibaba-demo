1c12d608bd59be223a53b9303697c1a35b532f89
liyi <liyi@glasssix.com>
2022-11-15 15:08:41 +0800

    Nacos

--- order/src/main/java/com/yibao/order/OrderApplication.java
+++ order/src/main/java/com/yibao/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.yibao.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @MapperScan(value = "com.yibao.order.mapper")
+@EnableDiscoveryClient
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
--- user/src/main/java/com/yibao/user/UserCenterApplication.java
+++ user/src/main/java/com/yibao/user/UserCenterApplication.java
@@ -3,9 +3,11 @@ package com.yibao.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan(value = "com.yibao.user.mapper")
+@EnableDiscoveryClient
 public class UserCenterApplication {
 
 	public static void main(String[] args) {
