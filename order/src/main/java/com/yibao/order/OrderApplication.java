10f87ff28d78745767c2242ed6f68ebb61393535
liyi <liyi@glasssix.com>
2022-11-16 10:56:43 +0800

    dubbo

--- order/src/main/java/com/yibao/order/OrderApplication.java
+++ order/src/main/java/com/yibao/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.yibao.order;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @MapperScan(value = "com.yibao.order.mapper")
 @EnableDiscoveryClient
+@EnableDubbo
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
--- order/src/main/java/com/yibao/order/service/impl/OrderFormServiceImp.java
+++ order/src/main/java/com/yibao/order/service/impl/OrderFormServiceImp.java
@@ -42,23 +42,5 @@ public class OrderFormServiceImp implements OrderFormService {
         return orderFormDto;
     }
 
-//    @Override
-//    public OrderFormDto findById(Integer id) throws Exception {
-//        // 1.查询订单信息
-//        OrderForm orderForm = orderMapper.selectById(id);
-//        // 2.获取用户 id
-//        Integer uid = orderForm.getUid();
-//        // 3.远程调用，获取用户名
-//        BaseResult<UserDto> baseResult = restTemplate.getForObject("http://localhost:9001/user/findById/{uid}", BaseResult.class, uid);
-//        // 3.1 数据转换
-//        String jsonObject= JSON.toJSONString(baseResult.getData());
-//        UserDto userDto = JSONObject.parseObject(jsonObject, UserDto.class);
-//        // 4.封装参数
-//        OrderFormDto orderFormDto = new OrderFormDto();
-//        BeanUtils.copyProperties(orderForm, orderFormDto);
-//        orderFormDto.setUsername(userDto.getUname());
-//        // 5.返回
-//        return orderFormDto;
-//    }
 
 }
